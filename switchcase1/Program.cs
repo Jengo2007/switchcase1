86358909bfa73a3e5127dd45950204253a1ecb66
Jengo2007 <zuyunovmagamed@gmail.com>
2024-11-24 13:33:28 +0300

    switchcase1

---
+++ switchcase1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace switchcase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number =Convert.ToInt32(Console.ReadLine());
+            switch (number) 
+            {
+                case 1 :
+                    Console.WriteLine("Понедельник");
+                    break;
+                case 2 :
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3 :
+                    Console.WriteLine("Среда");
+                    break;
+                case 4 :
+                    Console.WriteLine("четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("субота");
+                    break;
+                case 7:
+                    Console.WriteLine("воскресение");
+                    break;
+                default:
+                    Console.WriteLine("Введите число от 1 до 7");
+                    break;
+                    
+            }
+        }
+    }
+}
